9372f88a854fc6377d2c9755cbdc9bc49b5c38eb
Santhosh J <sxnthosh1501@gmail.com>
2025-09-25 19:11:37 +0530

    ATM

---
+++ atm.py
@@ -0,0 +1,17 @@
+print("welcome to the ATM ")
+pin=1501
+acc=1234567890
+while True:
+    pin=int(input("Enter the pin code"))
+    if pin==1501:
+        print("correct pin code")
+        
+    acc=int(input("Enter the acc number"))
+    if acc==1234567890:
+        print("withdrawl or deposite")
+        break
+    else:
+        print("invalied pin code")
+    
+
+        
